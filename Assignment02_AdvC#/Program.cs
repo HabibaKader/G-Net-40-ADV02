ee71a600751d7707844aa430a642070a19f2633b
HabibaKader <habibakader@outlook.com>
2026-03-19 01:35:17 +0200

    List<Product> products

--- Assignment02_AdvC#/Program.cs
+++ Assignment02_AdvC#/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Assignment02_AdvC_
+﻿using Assignment02_AdvC_.Classes;
+
+namespace Assignment02_AdvC_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            List<Product> products = new List<Product>
+            {
+            new Product("Laptop", "Electronics", 1200, 10),
+            new Product("Phone", "Electronics", 800, 25),
+            new Product("Headphones", "Electronics", 150, 40),
+            new Product("T-Shirt", "Clothing", 30, 100),
+            new Product("Jeans", "Clothing", 60, 50),
+            new Product("Chocolate", "Food", 5, 200),
+            new Product("Coffee Beans", "Food", 15, 80),
+            new Product("Novel", "Books", 20, 60),
+            new Product("C# Book", "Books", 45, 30)
+            };
+
+
+
+
         }
     }
 }
